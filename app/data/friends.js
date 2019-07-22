288913b0f43d2a8d4e48bd5d340a591f397969de
Claud Pod <claudiapollinger.cp@gmail.com>
2019-07-21 22:41:51 -0600

    putting out the best match on the page

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,37 +1,37 @@
 var friends = [{
-    name: "Claudia",
-    photo: "https://scontent.fapa1-1.fna.fbcdn.net/v/t31.0-8/15068307_10209780283697416_6020172313015239162_o.jpg?_nc_cat=111&_nc_oc=AQmV24FvMBwztbcAPQ4EJKAgb_1ciltOEbXX0Y6swsNdHIvQybZ_LVt4mYCPM2HXdU0&_nc_ht=scontent.fapa1-1.fna&oh=fe2d66bf984b8229eb70a379e8fea94d&oe=5DAB35E0",
+    name:"Claudia Pollinger",
+    photo: "./app/photos/claudia.png",
     scores: [5,5,5,3,3,3,5,3,4,5]
 },
 
 {
     name: "Chris Kekoa",
-    photo: "",
+    photo: "./app/photos/chriske.jpg",
     scores: [2,1, 5, 1, 5, 2, 4, 5, 1, 3]
   },
   {
     name: "Rachael Whitfield",
-    photo: "",
+    photo: "./app/photos/racael.jpg",
     scores: [2, 4, 1, 5, 5, 5, 2, 1, 2, 3]
   },
   {
     name: "Lindsay Chaplin",
-    photo: "",
+    photo: "./app/photos/lindsaych.jpeg",
     scores: [5, 5, 2, 2, 5, 5, 1, 3, 3, 4]
   },
   {
     name: "Kristen Christenson ",
-    photo: "",
+    photo: "./app/photos/KristenC.jpeg",
     scores: [4, 5, 1, 4, 3, 5, 1, 5, 1, 3]
   },
   {
     name: "Victoria Evens",
-    photo: "",
+    photo: "./app/photos/victoriae.jpg",
     scores: [4, 1, 3, 3, 1, 2, 3, 4, 1, 5]
   },
   {
     name: "Lovie Pollinger",
-    photo: "",
+    photo: "./app/photos/loviep.jpg",
     scores: [2, 5, 3, 1, 1, 2, 1, 5, 2, 3]
   }];
 
